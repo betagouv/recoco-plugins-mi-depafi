61c8c0cc68cf32af4e1e4ccd404ecd51a3fe0de5
Louis Pluvinage <louis.pluvinage0@gmail.com>
2026-07-08 16:08:34 +0200

    [feat] add next and previous button for photos in modal (logic + style)

--- plugin_mi_depafi/js/components/RealisationDetailView.js
+++ plugin_mi_depafi/js/components/RealisationDetailView.js
@@ -4,10 +4,12 @@ Alpine.data("realisationDetailView", (initialView = "detail") => ({
   currentView: initialView,
   photos: [],
   photoToDisplay: null,
+  totalPhotos: 0,
 
   init() {
     const photosData = document.getElementById("realisation-photos-data").textContent;
     this.photos = JSON.parse(photosData);
+    this.totalPhotos = this.photos.length;
   },
 
   showView(view) {
@@ -29,5 +31,17 @@ Alpine.data("realisationDetailView", (initialView = "detail") => ({
     this.photoToDisplay =
       index === -1 ? null : { ...this.photos[index], number: index + 1 };
     return this.photoToDisplay;
+  },
+
+  nextPic() {
+    if (this.photoToDisplay && this.photoToDisplay.number < this.totalPhotos) {
+      this.getPhotoToDisplay(this.photos[this.photoToDisplay.number].id);
+    }
+  },
+
+  previousPic() {
+    if (this.photoToDisplay && this.photoToDisplay.number > 1) {
+      this.getPhotoToDisplay(this.photos[this.photoToDisplay.number - 2].id);
+    }
   },
 }));
