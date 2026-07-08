1aaedce0a777e9c02198568fdd12e7589950f0b7
Louis Pluvinage <louis.pluvinage0@gmail.com>
2026-07-08 12:36:55 +0200

    [feat] add logic to change modal content and add more specific context from django to display photo and their number in array

--- plugin_mi_depafi/js/components/RealisationDetailView.js
+++ plugin_mi_depafi/js/components/RealisationDetailView.js
@@ -2,12 +2,32 @@ import Alpine from "alpinejs";
 
 Alpine.data("realisationDetailView", (initialView = "detail") => ({
   currentView: initialView,
+  photos: [],
+  photoToDisplay: null,
+
+  init() {
+    const photosData = document.getElementById("realisation-photos-data").textContent;
+    this.photos = JSON.parse(photosData);
+  },
 
   showView(view) {
+    if (view === "photos" && this.photos.length) {
+      this.getPhotoToDisplay(this.photos[0].id);
+    }
     this.currentView = view;
   },
 
   isView(view) {
     return this.currentView === view;
+  },
+
+  getPhotoToDisplay(photoId) {
+    if (this.currentView != "photos") {
+      this.currentView = "photos";
+    }
+    const index = this.photos.findIndex((photo) => photo.id === photoId);
+    this.photoToDisplay =
+      index === -1 ? null : { ...this.photos[index], number: index + 1 };
+    return this.photoToDisplay;
   },
 }));
