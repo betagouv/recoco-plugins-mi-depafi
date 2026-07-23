bf9db3ed55beef778f5b131b0cc5d85eb4d3c311
Guillaume Libersat <glibersat@sigill.org>
2026-07-23 11:22:33 +0200

    feat(brevo): add the template hook so it appears in the admin

--- plugin_mi_depafi/apps.py
+++ plugin_mi_depafi/apps.py
@@ -15,6 +15,8 @@ class PluginMiDepafiConfig(AppConfig):
 
         from . import signals as plugin_signals  # noqa: F401 - registers signal receivers
 
+        self._register_digest_email_template()
+
         project_signals.project_validated.connect(
             plugin_signals.notify_staff_on_project_validated
         )
@@ -38,3 +40,24 @@ class PluginMiDepafiConfig(AppConfig):
                 return f"{obj.project.name} {obj.partners} {obj.resource.title}"
 
         watson.register(Realisation, adapter_cls=RealisationSearchAdapter)
+
+    def _register_digest_email_template(self):
+        """Add the digest template to EmailTemplate's choices so it appears in the admin.
+
+        `EmailTemplate.name` is a core model field restricted to
+        `communication.constants.TPL_CHOICES`. Plugins have no hook to extend it, so we
+        patch the choices in place at startup.
+        """
+        from recoco.apps.communication import constants as communication_constants
+        from recoco.apps.communication.models import EmailTemplate
+
+        from .digests import TEMPLATE_NAME
+
+        choice = (TEMPLATE_NAME, "MI - Résumé des nouvelles réalisations")
+        if choice in communication_constants.TPL_CHOICES:
+            return
+
+        communication_constants.TPL_CHOICES += (choice,)
+        EmailTemplate._meta.get_field(
+            "name"
+        ).choices = communication_constants.TPL_CHOICES
