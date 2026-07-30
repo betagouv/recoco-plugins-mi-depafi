97f2bc49750ecb9d3c422243b91bdd1faecac37a
Guillaume Libersat <glibersat@sigill.org>
2026-07-30 11:27:21 +0200

    chore(buildsys): experiment with a new, independant, Vite build so plugins can be loaded at deploy time instead of pre-deploy.

--- plugin_mi_depafi/js/apps/realisationListCrm.js
+++ plugin_mi_depafi/js/apps/realisationListCrm.js
@@ -1,3 +1,8 @@
-import "@core/css/crm/projectList.scss";
+// TODO(import-map): this plugin now builds independently, so @core/css/*
+// can no longer be resolved at build time. projectList styles are missing
+// until core exposes a shared import map for its stylesheets.
+// import "@core/css/crm/projectList.scss";
 import "../components/RealisationListCrm";
+// TODO(import-map): left as an external bare import - resolves at runtime
+// once core exposes an import map entry for "@core/js/components/MultiSelect2.js".
 import "@core/js/components/MultiSelect2.js";
--- plugin_mi_depafi/js/components/RealisationResourceSelect.js
+++ plugin_mi_depafi/js/components/RealisationResourceSelect.js
@@ -1,4 +1,6 @@
 import Alpine from "alpinejs";
+// TODO(import-map): left as an external bare import - resolves at runtime
+// once core exposes an import map entry for "@core/js/utils/select-a11y".
 import Select from "@core/js/utils/select-a11y";
 
 Alpine.data("realisationResourceSelect", () => ({
